1031d3e31205a12ceecda84bae55cd1ae7244c71
Reza Rahem <101524351+rezarahem@users.noreply.github.com>
2024-11-30 10:27:34 +0330

    up

--- bash-scripts/docker.sh
+++ bash-scripts/docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update
+sudo apt install docker-ce -y
+
+
 # Install Docker Compose
 sudo rm -f /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
