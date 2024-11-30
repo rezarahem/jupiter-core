20e89b62d30e5841724c37c76643f3bb87de7d18
rahem <reza.rahem224@gmail.com>
2024-11-30 21:23:27 +0330

    update setup

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
 # remove run.sh
-rm run.sh
+rm -f run.sh
 
 # Prompt for variables
 read -p "Enter the domain name: " DOMAIN_NAME
 read -p "Enter your email: " EMAIL
+read -p "Enter your repo: " REPO_URL
+read -p "Enter yout app name: " APP_DIR
 
-# Check if inputs are empty
-if [[ -z "$DOMAIN_NAME" || -z "$EMAIL" ]]; then
-  echo "Error: Missing domain or email."
+cat <<EOF > ~/deploy.sh
+#!/bin/bash
+
+# Clone the Git repository
+if [ -d "$APP_DIR" ]; then
+  echo "Directory $APP_DIR already exists. Pulling latest changes..."
+  cd "$APP_DIR" && git pull
+  sudo docker-compose down
+  sudo docker-compose up --build -d
+else
+  echo "Cloning repository from $REPO_URL..."
+  git clone "$REPO_URL" "$APP_DIR"
+  cd "$APP_DIR"
+  sudo docker-compose up --build -d
+fi
+
+# Check if Docker Compose started correctly
+if ! sudo docker-compose ps | grep "Up"; then
+  echo "Docker containers failed to start. Check logs with 'docker-compose logs'."
   exit 1
 fi
+EOF
 
 
 # Mark scripts as executable
@@ -19,9 +38,9 @@ chmod +x docker.sh
 chmod +x nginx.sh 
 chmod +x deploy.sh
 
-# Run the scripts in sequence
+Run the scripts in sequence
 ./docker.sh
 ./nginx.sh "$DOMAIN_NAME" "$EMAIL"
 
-echo 'You're all set up! 👌'
+# echo 'You\'re all set up! 👌'
 
