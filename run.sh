a4dba728bc2df04df5b67fdbf3da6ce97d9607a5
rahem <reza.rahem224@gmail.com>
2024-12-08 18:15:34 +0330

    up

--- run.sh
+++ run.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 URL="https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/scripts.tar.gz"
-OUTPUT_FILE="scripts.tar.gz"
+FILE="scripts.tar.gz"
 
 # Download the tar.gz file
 echo "Downloading $URL..."
-curl -o "$OUTPUT_FILE" -L "$URL"
+curl -o "$FILE" -L "$URL"
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
-    echo "Downloaded successfully to $OUTPUT_FILE."
+    echo "Downloaded successfully to $FILE."
 else
     echo "Failed to download file."
     exit 1
 fi
 
 # Extract the tar.gz file into the current directory
-echo "Extracting $OUTPUT_FILE to the current directory..."
-tar -xzf "$OUTPUT_FILE"
+echo "Extracting $FILE to the current directory..."
+tar -xzf "$FILE"
 
 # Check if the extraction was successful
 if [ $? -eq 0 ]; then
     echo "Extraction completed successfully."
+    rm $FILE
 else
     echo "Extraction failed."
     exit 1
@@ -29,5 +30,4 @@ fi
 
 chmod +x ~/setup.sh
 
-# echo "🙌 Done!"
 
