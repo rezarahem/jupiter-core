5424cdf40cc40ce6eb64334070f38c9e5834f94a
Reza Rahem <101524351+rezarahem@users.noreply.github.com>
2024-12-08 12:58:31 +0330

    up

--- run.sh
+++ run.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# for url in "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/refresh.sh" "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/spinup.sh" "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/deploy.sh" "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/docker.sh" "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/nginx.sh" "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/setup.sh"  ; do echo -n "Downloading $(basename $url) ..."; curl -s -o ~/$(basename $url) "$url" && echo -e "\r$(printf ' %.0s' {1..50})\r✔  $(basename $url)"; [ "$(basename $url)" == "setup.sh" ] && chmod +x ~/setup.sh && echo "✔  Made setup.sh executable"; done; echo "🙌 Done!"
+URL="https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/scripts.tar.gz"
+OUTPUT_FILE="scripts.tar.gz"
 
+# Download the tar.gz file
+echo "Downloading $URL..."
+curl -o "$OUTPUT_FILE" -L "$URL"
 
-# Define an array of URLs to download
-urls=(
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/refresh.sh"
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/spinup.sh"
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/deploy.sh"
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/docker.sh"
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/nginx.sh"
-  "https://raw.githubusercontent.com/rezarahem/jupiter-core/refs/heads/sh/setup.sh"
-)
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Downloaded successfully to $OUTPUT_FILE."
+else
+    echo "Failed to download file."
+    exit 1
+fi
 
-# Loop through each URL
-for url in "${urls[@]}"; do
-  # Get the filename from the URL
-  filename=$(basename "$url")
+# Extract the tar.gz file into the current directory
+echo "Extracting $OUTPUT_FILE to the current directory..."
+tar -xzf "$OUTPUT_FILE"
 
-  # Start downloading the file
-  echo -n "Downloading $filename ..."
-
-  # Use curl to download the file and save it to the home directory
-  curl -s -o ~/"$filename" "$url" && \
-
-  # If the download is successful, print a success message
-  echo -e "\r$(printf ' %.0s' {1..50})\r✔  $filename"
-
-done
+# Check if the extraction was successful
+if [ $? -eq 0 ]; then
+    echo "Extraction completed successfully."
+else
+    echo "Extraction failed."
+    exit 1
+fi
 
 chmod +x ~/setup.sh
 
 echo "🙌 Done!"
+
